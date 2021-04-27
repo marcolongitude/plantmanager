be0da2cf09905e65290593dab59950365f041bb5
MARCO AURELIO <marcocpdti@gmail.com>
2021-04-26 23:28:27 -0300

    Aula 03 nlw 5 desafio astronautas

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,12 +5,23 @@ import {
   Image,
   View
 } from 'react-native';
+
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+
+import userImg from '../assets/marco.jpeg';
+
 import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 
 export function Header(){
   return (
     <View style={styles.container}> 
+      <View>
+        <Text style={styles.greeting}>Olá, </Text>
+        <Text style={styles.userName}>Rodrigo</Text>
+      </View>
 
+      <Image source={userImg} style={styles.image}/>
     </View>
   )
 }
@@ -23,6 +34,22 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     alignItems: 'center',
     paddingVertical: 20,
-    backgroundColor: colors.red
+    marginTop: getStatusBarHeight(),
+  },
+  image:{
+    width: 70,
+    height: 70,
+    borderRadius: 35
+  },
+  greeting: {
+    fontSize: 32,
+    color: colors.heading,
+    fontFamily: fonts.text
+  },
+  userName: {
+    fontSize: 32,
+    fontFamily: fonts.heading,
+    color: colors.heading,
+    lineHeight: 40
   }
 })
